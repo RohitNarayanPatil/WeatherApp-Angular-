3896f5a6c6beede3050b23ced34189b937bb5e29
RohitNarayanPatil <rnprohitpatil@gmail.com>
2024-04-16 10:01:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WeatherApp';
-}
+  city: string = '';
+  weatherData: any;
+  showImage: boolean = true;
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city?: string) {
+    if (city) {
+      this.http.get(`https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`, {
+        headers: {
+          'X-RapidAPI-Key': '9037939a91msh6613bfac2da4929p15ee2cjsn0cee449337cd',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+      }).subscribe((data: any) => {
+        this.weatherData = data;
+        this.showImage = false; // Hide the image after getting the weather data
+        console.warn("weatherData", data);
+      });
+    }
+  }}
